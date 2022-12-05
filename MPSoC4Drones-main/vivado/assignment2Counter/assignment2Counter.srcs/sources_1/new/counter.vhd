f2224952ac30b30c2365b5f618d691a3ed708fc2
beabo22 <beabo22@noreply>
2022-12-05 13:56:56 +0100

    fix: Fixing counter size and removing active

--- MPSoC4Drones-main/vivado/assignment2Counter/assignment2Counter.srcs/sources_1/new/counter.vhd
+++ MPSoC4Drones-main/vivado/assignment2Counter/assignment2Counter.srcs/sources_1/new/counter.vhd
@@ -35,28 +35,21 @@ use IEEE.NUMERIC_STD.ALL;
 entity counter is
     Port ( clk : in STD_LOGIC;
            rst : in STD_LOGIC;
-           counter_out : out STD_LOGIC_VECTOR (24 downto 0);
-           active : out STD_LOGIC);
+           counter_out : out STD_LOGIC_VECTOR (23 downto 0));
 end counter;
 
 architecture Behavioral of counter is
 
-signal sign_counter :   UNSIGNED(24 downto 0) := (OTHERS =>'0');
-signal sign_active :   STD_LOGIC := '0';
+signal sign_counter :   UNSIGNED(23 downto 0) := (OTHERS =>'0');
 begin
 mainProcess: process(clk,rst)
 begin
     if (rst='1') then
         sign_counter <= (OTHERS =>'0');
-        sign_active <= '0';
     elsif(rising_edge(clk)) then
-        if(sign_active =  '0') then
-            sign_active <= '1';
-        end if;
         sign_counter <= sign_counter + 1;
     end if;
     counter_out <= STD_LOGIC_VECTOR(sign_counter);
-    active <= sign_active;
 end process ; -- mainProcess
 
 end Behavioral;
